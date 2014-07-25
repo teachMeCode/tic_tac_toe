d59f1431c843b86ad82b588bafbd12e2097c049b
teachMeCode <brian.purcell7@gmail.com>
2014-07-24 23:17:14 -0700

    Added Firebase logic

--- app.js
+++ app.js
@@ -2,24 +2,53 @@ var myApp = angular.module("myApp", ["firebase"]);
 
 myApp.controller("myController", function ($scope, $firebase) {
 
-    var player = $scope.player = 1;
-    var clickCount = $scope.clickCount = 0;
+//-------------- Firebase ----------------------------------------------------------------------
+
+    var tttRef = new Firebase("https://bsp-wdi-ttt.firebaseio.com");
+
+    $scope.remoteClickCount = $firebase(new Firebase("https://bsp-wdi-ttt.firebaseio.com/" + "/clickCount"));
+    $scope.remoteClickCount.$bind($scope, "clickCount");
+    $scope.$watch('clickCount', function(){
+        console.log("Rad!");
+    });
+    
+    $scope.ttt = $firebase(tttRef);
+
+
+    $scope.updateClick  = function() {
+        tttRef.push({player1:$scope.player1});
+        tttRef.push({player2:$scope.player2});
+    }; //End of function
+
+// -------------- Player variables ----------------------------------------------------------
+
+    $scope.player = 1;
+    $scope.clickCount = 0;
+
+    var resetPlayer = function() {
+        $scope.player = 1;
+    }
+
+    var resetClickCount = function() {
+        $scope.clickCount = 0;
+    }
+  
   
 // ----------- Check box function --------------------------------------------------------------   
     $scope.clickBtn = function(btn) {
-      if (player == 1) {
+      if ($scope.player == 1) {
         document.getElementById(btn).value = "X";
         document.getElementById(btn).disabled = "disabled"; //This is to prevent multiple clicks
-        player -=1; //This causes player not to be 0
-        clickCount +=1;
-        document.getElementById("clicker").innerHTML = clickCount;
+        $scope.player -=1; //This causes player not to be 0
+        $scope.clickCount +=1;
+        document.getElementById("clicker").innerHTML = $scope.clickCount;
         $scope.winner();
       } else {
         document.getElementById(btn).value = "O";
         document.getElementById(btn).disabled = "disabled"; //This is to prevent multiple clicks
-        player +=1; //This causes player to be 1 again
-        clickCount +=1;
-        document.getElementById("clicker").innerHTML = clickCount;
+        $scope.player +=1; //This causes player to be 1 again
+        $scope.clickCount +=1;
+        document.getElementById("clicker").innerHTML = $scope.clickCount;
         $scope.winner(); 
         } 
       }
@@ -29,67 +58,82 @@ myApp.controller("myController", function ($scope, $firebase) {
       if (document.getElementById("btn1").value == "X" && 
         document.getElementById("btn2").value == "X" && 
         document.getElementById("btn3").value == "X" ) { 
-        alert("X is the Winner!");
+        alert($scope.player1 + " is the Winner!", "WINNER ALERT");
         alert("Game Over! Please Play again : )");
         clearBoard();
-        //document.getElementById("intro").innerHTML = "page-scroll";
-        //window.location.hash = "#intro";
+        resetPlayer();
+        resetClickCount();
+        document.getElementById("clicker").innerHTML = 0;
       } else if 
         (document.getElementById("btn1").value == "X" && 
         document.getElementById("btn4").value == "X" && 
         document.getElementById("btn7").value == "X" ) {
-        alert("X is the Winner!");
+        alert($scope.player1 + " is the Winner!");
         alert("Game Over! Please Play again : )")
         clearBoard();
-        //window.location.hash = "#intro"; 
+        resetPlayer();
+        resetClickCount();
+        document.getElementById("clicker").innerHTML = 0; 
       } else if 
         (document.getElementById("btn7").value == "X" && 
         document.getElementById("btn8").value == "X" && 
         document.getElementById("btn9").value == "X" ) {
-        alert("X is the Winner!");
+        alert($scope.player1 + " is the Winner!");
         alert("Game Over! Please Play again : )")
         clearBoard();
-        // window.location.hash = "#intro";
+        resetPlayer();
+        resetClickCount();
+        document.getElementById("clicker").innerHTML = 0;
       } else if 
         (document.getElementById("btn3").value == "X" && 
         document.getElementById("btn6").value == "X" && 
         document.getElementById("btn9").value == "X" ) {
-        alert("X is the Winner!");
+        alert($scope.player1 + " is the Winner!");
         alert("Game Over! Please Play again : )")
-        clearBoard(); 
-        window.location.hash = "#intro";
+        clearBoard();
+        resetPlayer();
+        resetClickCount();
+        document.getElementById("clicker").innerHTML = 0;
       } else if 
         (document.getElementById("btn1").value == "X" && 
         document.getElementById("btn5").value == "X" && 
         document.getElementById("btn9").value == "X" ) {
-        alert("X is the Winner!"); 
+        alert($scope.player1 + " is the Winner!"); 
         alert("Game Over! Please Play again : )")
         clearBoard();
-        // window.location.hash = "#intro";
+        resetPlayer();
+        resetClickCount();
+        document.getElementById("clicker").innerHTML = 0;
       } else if 
         (document.getElementById("btn3").value == "X" && 
         document.getElementById("btn5").value == "X" && 
         document.getElementById("btn7").value == "X" ) {
-        alert("X is the Winner!"); 
+        alert($scope.player1 + " is the Winner!"); 
         alert("Game Over! Please Play again : )")
         clearBoard();
-        // window.location.hash = "#intro";
+        resetPlayer();
+        resetClickCount();
+        document.getElementById("clicker").innerHTML = 0;
       } else if 
         (document.getElementById("btn2").value == "X" && 
         document.getElementById("btn5").value == "X" && 
         document.getElementById("btn8").value == "X" ) {
-        alert("X is the Winner!"); 
+        alert($scope.player1 + " is the Winner!"); 
         alert("Game Over! Please Play again : )")
         clearBoard();
-        // window.location.hash = "#intro";
+        resetPlayer();
+        resetClickCount();
+        document.getElementById("clicker").innerHTML = 0;
       } else if 
         (document.getElementById("btn4").value == "X" && 
         document.getElementById("btn5").value == "X" && 
         document.getElementById("btn6").value == "X" ) {
-        alert("X is the Winner!"); 
+        alert($scope.player1 + " is the Winner!"); 
         alert("Game Over! Please Play again : )")
         clearBoard();
-        // window.location.hash = "#intro";
+        resetPlayer();
+        resetClickCount();
+        document.getElementById("clicker").innerHTML = 0;
       }
 
 //------------------------ O Winner -----------------------------------------------------
@@ -97,72 +141,92 @@ myApp.controller("myController", function ($scope, $firebase) {
         (document.getElementById("btn1").value == "O" && 
         document.getElementById("btn2").value == "O" && 
         document.getElementById("btn3").value == "O" ) {
-        alert("O is the Winner!"); 
+        alert($scope.player2 + " is the Winner!"); 
         alert("Game Over! Please Play again : )")
         clearBoard();
-        // window.location.hash = "#intro";
+        resetPlayer();
+        resetClickCount();
+        document.getElementById("clicker").innerHTML = 0;
       } else if 
         (document.getElementById("btn1").value == "O" && 
         document.getElementById("btn4").value == "O" && 
         document.getElementById("btn7").value == "O" ) {
-        alert("O is the Winner!");         
+        alert($scope.player2 + " is the Winner!");         
         alert("Game Over! Please Play again : )")
         clearBoard();
-        // window.location.hash = "#intro";
+        resetPlayer();
+        resetClickCount();
+        document.getElementById("clicker").innerHTML = 0;
       } else if 
         (document.getElementById("btn7").value == "O" && 
         document.getElementById("btn8").value == "O" && 
         document.getElementById("btn9").value == "O" ) {
-        alert("O is the Winner!"); 
+        alert($scope.player2 + " is the Winner!"); 
         alert("Game Over! Please Play again : )")
         clearBoard();
-        // window.location.hash = "#intro";
+        resetPlayer();
+        resetClickCount();
+        document.getElementById("clicker").innerHTML = 0;
       } else if 
         (document.getElementById("btn3").value == "O" && 
         document.getElementById("btn6").value == "O" && 
         document.getElementById("btn9").value == "O" ) {
-        alert("O is the Winner!"); 
+        alert($scope.player2 + " is the Winner!"); 
         alert("Game Over! Please Play again : )")
         clearBoard();
-        // window.location.hash = "#intro";
+        resetPlayer();
+        resetClickCount();
+        document.getElementById("clicker").innerHTML = 0;
       } else if 
         (document.getElementById("btn2").value == "O" && 
         document.getElementById("btn5").value == "O" && 
         document.getElementById("btn8").value == "O" ) {
-        alert("O is the Winner!"); 
+        alert($scope.player2 + " is the Winner!"); 
         alert("Game Over! Please Play again : )")
         clearBoard();
-        // window.location.hash = "#intro";
+        resetPlayer();
+        resetClickCount();
+        document.getElementById("clicker").innerHTML = 0;
       } else if 
         (document.getElementById("btn4").value == "O" && 
         document.getElementById("btn5").value == "O" && 
         document.getElementById("btn6").value == "O" ) {
-        alert("O is the Winner!"); 
+        alert($scope.player2 + " is the Winner!"); 
         alert("Game Over! Please Play again : )")
         clearBoard();
-        // window.location.hash = "#intro";
+        resetPlayer();
+        resetClickCount();
+        document.getElementById("clicker").innerHTML = 0;
       } else if 
         (document.getElementById("btn1").value == "O" && 
         document.getElementById("btn5").value == "O" && 
         document.getElementById("btn9").value == "O" ) {
-        alert("O is the Winner!"); 
+        alert($scope.player2 + " is the Winner!"); 
         alert("Game Over! Please Play again : )")
         clearBoard();
-        // window.location.hash = "#intro";
+        resetPlayer();
+        resetClickCount();
+        document.getElementById("clicker").innerHTML = 0;
       } else if 
         (document.getElementById("btn3").value == "O" && 
         document.getElementById("btn5").value == "O" && 
         document.getElementById("btn7").value == "O" ) {
-        alert("O is the Winner!"); 
+        alert($scope.player2 + " is the Winner!"); 
         alert("Game Over! Please Play again : )")
         clearBoard();
-        // window.location.hash = "#intro";
-      } else if (clickCount == 9) {
+        resetPlayer();
+        resetClickCount();
+        document.getElementById("clicker").innerHTML = 0;
+      } else if ($scope.clickCount == 9) {
         alert("You are not good, please get some lessons");
         clearBoard();
+        resetPlayer();
+        resetClickCount();
+        document.getElementById("clicker").innerHTML = 0;
       }
     } //End of winner 
 
+    
     // $scope.clearBoard = function() {
     //   document.getElementById('btn1').value = "";
     //   document.getElementById('btn2').value = "";
--- script.js
+++ script.js
@@ -12,13 +12,20 @@ function clearBoard() {
   document.getElementById("btn7").value = "";
   document.getElementById("btn8").value = "";
   document.getElementById("btn9").value = "";
-  load();  // this refreshes the page once the game is complete
+  //load();  // this refreshes the page once the game is complete
+  document.getElementById("btn1").disabled = false;
+  document.getElementById("btn2").disabled = false;
+  document.getElementById("btn3").disabled = false;
+  document.getElementById("btn4").disabled = false;
+  document.getElementById("btn5").disabled = false;
+  document.getElementById("btn6").disabled = false;
+  document.getElementById("btn7").disabled = false;
+  document.getElementById("btn8").disabled = false;
+  document.getElementById("btn9").disabled = false;
 }
 
 // This refreshes the board
-var load = function() {
-  window.location.reload(true);
-}
-
-
+// var load = function() {
+//   window.location.reload(true);
+// }
 
